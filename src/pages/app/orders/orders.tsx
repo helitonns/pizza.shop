84d6f72ed1769b47e5f6a76f72d639b4c2b30cc1
helitonns <helitonns@gmail.com>
2025-05-09 15:16:24 -0400

    Loading dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -7,6 +7,7 @@ import { useSearchParams } from "react-router-dom";
 import { z } from "zod";
 import { OrderTabelFilters } from "./order-table-filters";
 import { OrderTableRow } from "./order-table-row";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -20,7 +21,7 @@ export function Orders() {
     .transform(page => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const {data: result} = useQuery({
+  const {data: result, isLoading: isLoadingOrders} = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: ()=> getOrders({pageIndex, orderId, customerName, status: status === "all" ? null : status})
   });
@@ -56,6 +57,8 @@ export function Orders() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
+
                 {result && result.orders.map(order => {
                   return <OrderTableRow key={order.orderId} order={order}/>
                 })}
