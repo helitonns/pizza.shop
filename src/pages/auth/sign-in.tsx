a1de2435fcdaf6545df81015d7820192904c0714
helitonns <helitonns@gmail.com>
2025-05-13 18:00:49 -0400

    Teste E2E: Sign In

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -31,10 +31,12 @@ export function SignIn(){
   });
 
   async function handleSignIn(data: SignInForm){
-    console.log(data);
-    await authenticate({email: data.email});
-
-    toast.success("Enviamos um link de autenticação para seu e-mail");
+    try {
+      await authenticate({email: data.email});
+      toast.success("Enviamos um link de autenticação para seu e-mail");  
+    } catch (error) {
+      toast.error("Credenciais inválidas");
+    }
   }
 
   return(
